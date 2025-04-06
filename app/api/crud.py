a0ecc3d5f9b351f3cf18e2348b6e5e12abc6db94
vvetta <drpsih2004@gmail.com>
2025-04-06 21:28:45 +0300

    add expire time

--- app/api/crud.py
+++ app/api/crud.py
@@ -4,6 +4,7 @@ from fastapi import HTTPException
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from cryptography.fernet import Fernet
+from datetime import datetime, timedelta, timezone
 
 from api.log import logger
 from api.models import Secret
@@ -13,7 +14,9 @@ from api.schemas import SecretKeyMessage, StatusMessage, CreateSecretSchema, Bas
 
 if SECRET_KEY:
     cipher = Fernet(SECRET_KEY)
+    logger.info("Ключ шифрования успешно установлен!")
 else:
+    logger.warning("Ключ шифрования не найден! Установите ключ шифрования через переменную окружения!")
     raise HTTPException(status_code=500, detail="Something went wrong!")
 
 
@@ -21,10 +24,13 @@ async def add_secret_to_db(secret_payload: CreateSecretSchema,
                            session: AsyncSession) -> SecretKeyMessage:
     new_secret_payload = secret_payload.model_dump()
     new_secret_payload["secret"] = cipher.encrypt(new_secret_payload["secret"].encode())
+
     secret_key = str(uuid.uuid4())
 
     new_secret = Secret(**new_secret_payload, secret_key=secret_key)
     session.add(new_secret)
+
+    logger.info(new_secret.time_created)
 
     try:
         await session.commit()
@@ -33,7 +39,7 @@ async def add_secret_to_db(secret_payload: CreateSecretSchema,
 
     except Exception as e:
         await session.rollback()
-        logger.warning("Произошла ошибка при создании секрета!")
+        logger.warning(f"Произошла ошибка при создании секрета! {e}")
         raise HTTPException(status_code=400, detail="Something went wrong!")
 
 
@@ -45,7 +51,17 @@ async def get_secret_from_db(secret_key: str,
     if not secret:
         raise HTTPException(status_code=404, detail="Secret not Found")
 
-    if secret[0].num_of_readings == 0:
+    expire_flag = False
+
+    if secret[0].ttl_seconds != 0:
+        secret_time_created = datetime.fromisoformat(str(secret[0].time_created))
+        expire_time = secret_time_created + timedelta(seconds=secret[0].ttl_seconds)
+        now = datetime.now(timezone.utc)
+
+        if now > expire_time:
+            expire_flag = True
+
+    if secret[0].num_of_readings == 0 and not expire_flag:
 
         decrypted_secret = cipher.decrypt(secret[0].secret).decode()
 
@@ -65,12 +81,14 @@ async def delete_secret_from_db(secret_key: str,
     secret = secret.fetchone()
 
     if not secret:
+        logger.info("Секрет не был найден!")
         raise HTTPException(status_code=404, detail="Secret not Found")
 
-    await session.delete(secret)
+    await session.delete(secret[0])
 
     try:
         await session.commit()
+        logger.info("Секрет был успешно удален!")
     except Exception as e:
         await session.rollback()
         logger.warning("Произошла ошибка при удалении секрета!")
--- app/api/models.py
+++ app/api/models.py
@@ -1,11 +1,11 @@
-from sqlalchemy import String, Boolean, Integer
+from sqlalchemy import String, Boolean, Integer, LargeBinary
 from sqlalchemy.orm import mapped_column
 
 from api.database import BaseModel
 
 
 class Secret(BaseModel):
-    secret = mapped_column(String(length=255), nullable=False, unique=False)
+    secret = mapped_column(LargeBinary, nullable=False)
     secret_key = mapped_column(String(), nullable=False, unique=True)
     passphrase = mapped_column(String(length=255), nullable=True, unique=False)
     ttl_seconds = mapped_column(Integer(), nullable=True)
