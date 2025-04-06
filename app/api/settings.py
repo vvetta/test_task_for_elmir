d44e33228b9bf10d67971dbd8059e09c21cc5337
vvetta <drpsih2004@gmail.com>
2025-04-06 15:31:30 +0300

    rename env var

--- app/api/settings.py
+++ app/api/settings.py
@@ -6,5 +6,5 @@ load_dotenv()
 
 
 DATA_BASE_URL: str = (f"postgresql+asyncpg://{os.getenv("POSTGRES_USER")}:{os.getenv("POSTGRES_PASSWORD")}"
-                      f"@{os.getenv("POSTGRES_HOST")}:{os.getenv("POSTGRES_PORT")}/{os.getenv("POSTGRES_NAME")}")
+                      f"@{os.getenv("POSTGRES_HOST")}:{os.getenv("POSTGRES_PORT")}/{os.getenv("POSTGRES_DB")}")
 
