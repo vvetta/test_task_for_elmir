4d47f175cd8df4a1986a609cc58d74c28992f090
vvetta <drpsih2004@gmail.com>
2025-04-06 15:24:41 +0300

    add secret_key field to Secret model

--- app/api/models.py
+++ app/api/models.py
@@ -6,6 +6,7 @@ from api.database import BaseModel
 
 class Secret(BaseModel):
     secret = mapped_column(String(length=255), nullable=False, unique=False)
+    secret_key = mapped_column(String(), nullable=False, unique=True)
     passphrase = mapped_column(String(length=255), nullable=True, unique=False)
     ttl_seconds = mapped_column(Integer(), nullable=True)
 
